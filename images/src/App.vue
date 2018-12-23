104e11e22c12508c57a0117903cf15fa6d8618b9
Boyle, Dave <dave.boyle@investorsgroup.com>
2018-12-23 11:57:54 -0500

    [master] - Added boilerplate files for image app.

---
+++ images/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <AppHeader />
+    </div>
+</template>
+
+<script>
+
+    import AppHeader from './components/AppHeader';
+
+    export default {
+        name: 'App',
+        components: {
+            AppHeader
+        }
+    };
+</script>
+
+<style>
+    
+</style>
