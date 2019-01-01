c44fc1111aae6f45e853e9f8e6cd992bfa7011a8
Boyle, Dave <dave.boyle@investorsgroup.com>
2019-01-01 16:13:37 -0500

    [master] - Completed imgur app.

--- images/src/api/imgur.js
+++ images/src/api/imgur.js
@@ -19,5 +19,23 @@ export default {
                 Authorization: `Bearer ${token}`
             }
         });
+    },
+    uploadImages(images, token) {
+        //turn FileList into true array
+        const promises = Array.from(images).map(image => {
+            
+            const formData = new FormData();
+            //takes file reference and retrieves the actual file
+            formData.append('image', image);
+
+            return axios.post(`${ROOT_URL}/3/image`, formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+        });
+
+        //Make sure all promises have been resolved
+        return Promise.all(promises);
     }
 }
--- images/src/store/modules/images.js
+++ images/src/store/modules/images.js
@@ -1,4 +1,6 @@
-import api from '../../api/imgur'; 
+import api from '../../api/imgur';
+
+import { router } from '../../main';
 
 const state = {
     images: []
@@ -10,11 +12,29 @@ const getters = {
 
 const actions = {
     //all complicated business logic goes here
-    async fetchImages({ rootState, commit }) {
-        const { token } = rootState.auth;
-        const response  = await api.fetchImages(token);
-        
+    async fetchImages({
+        rootState,
+        commit
+    }) {
+        const {
+            token
+        } = rootState.auth;
+        const response = await api.fetchImages(token);
+
         commit('setImages', response.data.data);
+    },
+
+    //rootState access the root of the vuex store
+    async uploadImages({ rootState }, images) {
+        //get access token
+        const { token } = rootState.auth;
+
+        //call our API module to do the upload
+        await api.uploadImages(images, token);
+
+        //Redirect our user to IMageList component
+        router.push('/');
+
     }
 };
 
