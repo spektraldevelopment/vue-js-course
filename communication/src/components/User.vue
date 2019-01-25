67046131536af9d2e2ef69fac0a1199a1d3ca0cf
Boyle, Dave <dave.boyle@investorsgroup.com>
2019-01-24 20:17:05 -0500

    [master] - Added communication example.

---
+++ communication/src/components/User.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change My Name</button>
+        <p>Name is: {{name}}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :name="name" @nameWasReset="name = $event" :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :userAge="age" @ageWasEdited="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data() {
+            return {
+                name: "Dave",
+                age: 27
+            }
+        },
+        methods: {
+            changeName() {
+                this.name = "Breanne";
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scope>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ communication/src/components/UserDetail.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{switchName()}}</p>
+        <p>User Age: {{userAge}}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+
+    import { eventBus } from '../main';
+
+    export default {
+        props: {
+            name: String,
+            userAge: Number
+        },
+        methods: {
+            switchName() {
+                return this.name.split("").reverse().join("");
+            },
+            resetName() {
+                this.name = 'Dave';
+                this.$emit('nameWasReset', this.name);
+            }
+        },
+        created() {
+            eventBus.$on('ageWasEdited', (data) => {
+                this.userAge = data;
+            });
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
