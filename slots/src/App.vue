e3620df892dc3202add8486ae46be05a89b9dc8d
Boyle, Dave <dave.boyle@investorsgroup.com>
2019-01-29 13:49:05 -0500

    [master] - Added slots example.

---
+++ slots/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+
+                <keep-alive>
+                    <component :is="selectedComponent"></component>
+                </keep-alive>
+
+
+               <!-- <app-quote>
+                   <h2 slot="title">{{ quoteTitle }}</h2>
+                   <p slot="content">A wonderful quote!</p>
+               </app-quote> -->
+               <hr>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import Quote from './components/Quote';
+    import Author from './components/Author';
+    import New from './components/New';
+
+    export default {
+        data: function () {
+            return {
+                quoteTitle: 'The Quote!!!!',
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ slots/src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="title">
+            <slot name="title"></slot>
+            <span style="color: #ccc"><slot name="subtitle">The Subtitle</slot></span>
+        </div>
+        <hr>
+        <div>
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+
+    .title {
+        font-style: italic;
+    }
+</style>
